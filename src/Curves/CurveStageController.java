5d5e9adee4dab84b8c1e7bffa3f0f9c7dccb6722
Nils Grimmer <nilsgrimmer@web.de>
2015-06-19 07:26:53 +0200

    Grid displayed

--- src/Curves/CurveStageController.java
+++ src/Curves/CurveStageController.java
@@ -1,9 +1,28 @@
 package Curves;
 
 import Uniwork.Appl.NGCustomStageItem;
+import Uniwork.Visuals.NGDisplayView;
+import Uniwork.Visuals.NGGrid2DDisplayController;
 import Uniwork.Visuals.NGStageController;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
 
 public class CurveStageController extends NGStageController {
+
+    @FXML
+    private Canvas Layer0;
+
+    @Override
+    protected void CreateDisplayController() {
+        super.CreateDisplayController();
+        NGDisplayView dv = new NGDisplayView(Layer0.getWidth(), Layer0.getHeight());
+        NGGrid2DDisplayController dcgrid = new NGGrid2DDisplayController(Layer0, "Grid");
+        dcgrid.setView(dv);
+        dcgrid.GridDistance = 20;
+        dcgrid.GridColor = Color.DARKGRAY;
+        registerDisplayController(dcgrid);
+    }
 
     public CurveStageController() {
         this(null);
@@ -11,6 +30,7 @@ public class CurveStageController extends NGStageController {
 
     public CurveStageController(NGCustomStageItem aStageItem) {
         super(aStageItem);
+
     }
 
 }
