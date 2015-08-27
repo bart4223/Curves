ed29f55ab14cfad0372ea98e1f2a50c2cdfddc71
Nils Grimmer <nilsgrimmer@web.de>
2015-08-27 21:20:48 +0200

    Main help toolbox

---
+++ src/Curves/CurveToolboxItem.java
@@ -0,0 +1,13 @@
+package Curves;
+
+import Uniwork.Appl.NGCustomToolboxItem;
+import Uniwork.Appl.NGStageManager;
+import javafx.stage.Stage;
+
+public class CurveToolboxItem extends NGCustomToolboxItem {
+
+    public CurveToolboxItem(NGStageManager aStageManager, String aName, Stage aStage) {
+        super(aStageManager, aName, aStage);
+    }
+
+}
--- src/Curves/MainApplicationModule.java
+++ src/Curves/MainApplicationModule.java
@@ -3,6 +3,7 @@ package Curves;
 import Uniwork.Appl.NGCustomStageItem;
 import Uniwork.Appl.NGVisualApplicationModule;
 import Uniwork.Base.NGComponent;
+import Uniwork.UI.NGUIHelpToolboxContext;
 
 public class MainApplicationModule extends NGVisualApplicationModule {
 
@@ -14,10 +15,21 @@ public class MainApplicationModule extends NGVisualApplicationModule {
         item.setPosition(1000, 100);
     }
 
+    @Override
+    protected void registerObjectRequests() {
+        super.registerObjectRequests();
+        registerObjectRequest("Main", this, "Help", "ShowHelp");
+    }
+
     public MainApplicationModule(NGComponent aOwner, String aName) {
         super(aOwner, aName);
         FDescription = "Main";
         FStageManager.registerItemClass("Main", "Curves.MainStageItem");
+        FToolboxManager.registerItemClass("Help", "Uniwork.UI.NGUIHelpToolboxItem");
+    }
+
+    public void ShowHelp() {
+        FToolboxManager.ShowToolbox("Help", String.format("%s.Help", getDescription()), new NGUIHelpToolboxContext(Application.LoadResourceFileContent("help/welcome.txt")));
     }
 
 }
